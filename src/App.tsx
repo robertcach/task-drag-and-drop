f8152c6c15b05a97c72855aaf7ddc638663db1b9
Robert Cabrera <cabrera.rch@gmail.com>
2022-06-10 18:21:58 +0200

    feat: form is working with interface props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "./App.scss";
 import InputField from "./components/InputField/InputField";
 
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  
   return (
     <div className="header">
       <h1 className="header__title">Taskify</h1>
-      <InputField />
+      <InputField todo={todo} setTodo= {setTodo}  />
     </div>
   );
 }
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,9 +1,20 @@
 import "./InputField.scss"
 
-const InputField: React.FC = () => {
+interface InputProps {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField: React.FC<InputProps> = ({ todo, setTodo}) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+      <input
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+        value={todo}
+        onChange={e => setTodo(e.target.value)}
+      />
       <button className="input__submit" type="submit">Go</button>
     </form>
   )
