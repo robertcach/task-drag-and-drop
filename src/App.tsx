5604334f66d437277ee6e9060d9dc0970504e4bc
Robert Cabrera <cabrera.rch@gmail.com>
2022-06-10 17:38:08 +0200

    feat: add form to new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import "./App.scss";
+import InputField from "./components/InputField/InputField";
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <h1 className="header__title">Taskify</h1>
+      <InputField />
     </div>
   );
 }
---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,12 @@
+import "./InputField.scss"
+
+const InputField: React.FC = () => {
+  return (
+    <form className="input">
+      <input type="input" placeholder="Enter a task" className="input__box" />
+      <button className="input__submit" type="submit">Go</button>
+    </form>
+  )
+}
+
+export default InputField;
